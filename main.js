700ace11f605591748f516529f4d0ef2d8e82b6f
sreenivasani <146092449+sreenivasani@users.noreply.github.com>
2023-09-26 18:08:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var points=0;
+function decrease(){
+    points=points-5;
+     document.getElementById("points").textContent=points;
+    
+}
+    function increase(){
+        points=points+5;
+         document.getElementById("points").textContent=points;
+    
+}
